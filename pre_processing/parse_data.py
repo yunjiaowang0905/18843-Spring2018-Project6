232969d69b7673aa5fd08ca1b55f04ee4689a26c
yunjiao <yunjiao1@andrew.cmu.edu>
2018-02-12 13:37:03 -0800

    check pm2.5 range & filter pm2.5

--- pre_processing/parse_data.py
+++ pre_processing/parse_data.py
@@ -5,6 +5,8 @@ import yaml
 import pandas as pd
 import glob
 import matplotlib.pyplot as plt
+import csv
+from datetime import datetime, timedelta
 
 
 def readFiles(path):
@@ -23,47 +25,61 @@ def filterPos(df, min_lat, max_lat, min_lon, max_lon):
 def filterTime(df, min_time, max_time):
     mask = df[0] >= min_time
     mask &= df[0] <= max_time
-    if mask is False:
-        print(df[mask])
+    print(df[~mask])
     return df[mask]
 
 def filterPM2d5(df, min_pm2d5, max_pm2d5):
-    mask = df[0] >= min_pm2d5
-    mask &= df[0] <= max_pm2d5
+    mask = df[10] >= min_pm2d5
+    mask &= df[10] <= max_pm2d5
+    print(df[~mask])
+    df[~mask].to_csv('filtered_pm2d5.csv', index = False)
     return df[mask]
 
 def filterCO(df, min_co, max_co):
-    mask = df[0] >= min_co
-    mask &= df[0] <= max_co
+    mask = df[1] >= min_co
+    mask &= df[1] <= max_co
+    print(df[~mask])
     return df[mask]
 
 def filterCO2(df, min_co2, max_co2):
-    mask = df[0] >= min_co2
-    mask &= df[0] <= max_co2
+    mask = df[2] >= min_co2
+    mask &= df[2] <= max_co2
+    print(df[~mask])
     return df[mask]
 
 def filterNO2(df, min_no2, max_no2):
-    mask = df[0] >= min_no2
-    mask &= df[0] <= max_no2
+    mask = df[6] >= min_no2
+    mask &= df[6] <= max_no2
+    print(df[~mask])
     return df[mask]
 
+def datetime2matlabdn(dt):
+   mdn = dt + timedelta(days = 366)
+   frac_seconds = (dt-datetime.datetime(dt.year,dt.month,dt.day,0,0,0)).seconds / (24.0 * 60.0 * 60.0)
+   frac_microseconds = dt.microsecond / (24.0 * 60.0 * 60.0 * 1000000.0)
+   return mdn.toordinal() + frac_seconds + frac_microseconds
 
 # Get parameters
 params_fn = "parameters.yaml"
 params_f = open(params_fn)
 params = yaml.load(params_f)
-params['Max_time'] = float(params['Max_time'])
-params['Min_time'] = float(params['Min_time'])
 
 # Read files
-paths = readFiles('../data/sensor_data/*.mat')
+paths = readFiles('sensor_data/*.mat')
 list_a = pd.DataFrame()
 for path in paths:
     mat_contents = sio.loadmat(path)
     list_a = list_a.append(pd.DataFrame(mat_contents['DATA']))
 
+print(len(list_a))
+
 # Check ranges
 list_a = filterPos(list_a, 18, 24, 112, 115)
+list_a = filterPM2d5(list_a, float(params['Min_pm2d5']), float(params['Max_pm2d5']))
+list_a = filterTime(list_a, float(params['Min_time']), float(params['Max_time']))
+list_a = filterCO(list_a, float(params['Min_co']), float(params['Max_co']))
+list_a = filterCO2(list_a, float(params['Min_co2']), float(params['Max_co2']))
+list_a = filterNO2(list_a, float(params['Min_no2']), float(params['Max_no2']))
 
 # Draw plots
 max_lantitude = list_a[4].max()
