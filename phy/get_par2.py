baad84eb7a67addb2b7312c7ea3ffa8515a93531
youx <you.xu@west.cmu.edu>
2018-02-22 20:59:18 -0800

    [Fix] Use matlab-cvx_solve_u

--- phy/get_par2.py
+++ phy/get_par2.py
@@ -1,12 +1,17 @@
 import numpy as np
 from scipy.linalg import expm, solve
 from scipy.sparse import bsr_matrix
+from scipy.io import savemat
 from scipy.io import loadmat
 from getA_c import getA_c
-from cvx_solve_u import cvx_solve_u
+import matlab.engine
+#from cvx_solve_u import cvx_solve_u
 
 def get_par2(K, vx, vy, l, dt, x_cell):
     m, n = x_cell[0].shape
+    print(len(x_cell.tolist()))
+    print(len(x_cell[0].tolist()))
+    print(len(x_cell[0][0].tolist()))
     V_x = np.zeros((m,n)) + vx
     V_y = np.zeros((m,n)) + vy
 
@@ -19,10 +24,12 @@ def get_par2(K, vx, vy, l, dt, x_cell):
     A1[np.where(np.absolute(A1) < 1e-3 * np.amax(np.absolute(A1)))] = 0
     B[np.where(np.absolute(B) < 1e-3 * np.amax(np.absolute(B)))] = 0
 
-    coe_matrix = bsr_matrix(A1)
-    B = bsr_matrix(B)
+    # coe_matrix = bsr_matrix(A1, (1024,1024))
+    # B = bsr_matrix(B, (1024,1024))
 
-    u = cvx_solve_u(coe_matrix, B, x_cell)
+    eng = matlab.engine.start_matlab()
+    u = eng.cvx_solve_u(A1.tolist(), B.tolist(), x_cell.tolist())
+    # u = cvx_solve_u(coe_matrix, B, x_cell)
     u_mat = np.transpose(np.reshape(u, (n,m)))
     return u_mat
 
