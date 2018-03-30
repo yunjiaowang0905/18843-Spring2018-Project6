311a7f7f4491cdf6d011cb9d6d558edcc8a6d871
gardenia22 <nanshu.wang@gmail.com>
2018-03-29 18:17:11 -0700

    remove dependency to matlab engine

--- ipsn2018/util/get_par2.py
+++ ipsn2018/util/get_par2.py
@@ -1,16 +1,8 @@
 import numpy as np
-from scipy.linalg import expm, solve
-from scipy.sparse import bsr_matrix
-from scipy.io import savemat
-from scipy.io import loadmat
-from getA_c import getA_c
-import matlab.engine
 from cvx_solve_u import cvx_solve_u
 
 def get_par2(A, B, x_cell):
     m, n = x_cell[0].shape
-    # eng = matlab.engine.start_matlab()
-    # u = eng.cvx_solve_u(A.tolist(), B.tolist(), x_cell)
     u = cvx_solve_u(A, B, x_cell)
     u_mat = np.transpose(np.reshape(u, (n,m)))
     return u_mat
